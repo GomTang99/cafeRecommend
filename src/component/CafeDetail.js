bffbaea4a77d3746177f0f174cc968e0688a7416
Juneyoung Park <parkjuneyoung194786@gmail.com>
2024-11-22 14:19:16 +0900

    24/11/22 카카오맵 지도 API 연동

--- src/component/CafeDetail.js
+++ src/component/CafeDetail.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import styles from './CafeDetail.module.css';
 import { Link, useParams } from "react-router-dom";
 
+const { kakao } = window;
+
 export default function CafeDetail() {
     const { id } = useParams();
+    const container = useRef(null); // 지도 데이터 접근
 
     //카페 데이터 (예시 데이터)
     const cafes = [
@@ -15,6 +18,16 @@ export default function CafeDetail() {
 
     // 선택한 카페 정보 찾기
     const cafe = cafes.find(cafe => cafe.id === parseInt(id));
+
+    useEffect(() => {
+        const position = new kakao.maps.LatLng(33.450701, 126.570667)
+        const options = {
+            center: position,
+            level: 3,
+        };
+        const map = new kakao.maps.Map(container.current, options);
+    }, []);
+
 
     if(!cafe) {
         return <p>카페 정보를 찾을 수 없습니다.</p>
@@ -32,6 +45,9 @@ export default function CafeDetail() {
                     <h2 className={styles.cafeTitle}>{cafe.name}</h2>
                     <p className={styles.cafeInfo}>{cafe.region} | {cafe.category}</p>
                     <p className={styles.cafeDescription}>{cafe.description}</p>
+
+                        <div style={{ width: "500px", height: "500px" }} ref={container}></div>
+
                     <Link to="/" className={styles.backLink}>뒤로가기</Link>
                 </div>
             </div>
